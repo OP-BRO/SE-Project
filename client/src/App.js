546cc9996384bcb21ad1bc15432265a4c4dad3e1
SwamiKedari <cs20b048@iittp.ac.in>
2022-09-15 19:48:06 +0530

    added the classroom

--- client/src/App.js
+++ client/src/App.js
@@ -3,11 +3,12 @@ import Feedback from './components/Feedback';
 import Mainpage from './components/Mainpage';
 import Event from './components/Event'
 import Alumni from './components/Alumni'
+import Classroom from './components/Classroom'
 
 function App() {
   return (
     <div className="App">
-      <Alumni/>
+      <Classroom/>
     </div>
   );
 }
