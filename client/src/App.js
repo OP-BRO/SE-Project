fb6a326add233f33670429e7a1e169873cb86606
SwamiKedari <cs20b048@iittp.ac.in>
2022-09-15 20:34:27 +0530

    bap file

--- client/src/App.js
+++ client/src/App.js
@@ -4,11 +4,12 @@ import Mainpage from './components/Mainpage';
 import Event from './components/Event'
 import Alumni from './components/Alumni'
 import Classroom from './components/Classroom'
+import Bap from './components/Bap'
 
 function App() {
   return (
-    <div className="App">
-      <Mainpage/>
+    <div className="Mainpage">
+      <Bap/>
     </div>
   );
 }
