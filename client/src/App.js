76f55f99036146f20d7aab5ed39b07a467103b12
Abhay Krishna <cs20b001@iittp.ac.in>
2022-09-15 19:28:20 +0530

    scholarship:

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
+import Attendance from './components/Attendance';
 import Feedback from './components/Feedback';
+import Mainage from './components/Mainpage';
 import Mainpage from './components/Mainpage';
+import Scholarship from './components/Scholarship';
 
 function App() {
   return (
     <div className="App">
-      <Mainpage/>
+      <Attendance/>
     </div>
   );
 }
